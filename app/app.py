424d9b2068e99858d3e4c54d152af5d288541993
DylanDSP10 <141286179+DylanDSP10@users.noreply.github.com>
2024-08-25 12:41:53 -0300

    <upgrade>  conexión de la base de datos

--- app/app.py
+++ app/app.py
@@ -1,25 +1,28 @@
 from flask import Flask, render_template
-from flask_mysql_connector import MySQL
+import mysql.connector
 
 app = Flask(__name__)
 
 # Configurar la base de datos MySQL en Hostinger
-app.config['MYSQL_HOST'] = 'red-rail-734138.hostingersite.com'   # Reemplaza con tu host en Hostinger
-app.config['MYSQL_USER'] = 'u653663436_root'      # Reemplaza con el nombre de usuario que creaste
-app.config['MYSQL_PASSWORD'] = 'Proyectowebs1'  # Reemplaza con la contraseña que configuraste
-app.config['MYSQL_DATABASE'] = 'u653663436_barberx'  # Reemplaza con el nombre de tu base de datos
+db_config = {
+    'host': 'red-rail-734138.hostingersite.com',          # Reemplaza con tu host en Hostinger
+    'user': 'u653663436_root',             # Reemplaza con el nombre de usuario que creaste
+    'password': 'Proyectowebs1',      # Reemplaza con la contraseña que configuraste
+    'database': 'u653663436_barberx'   # Reemplaza con el nombre de tu base de datos
+}
 
-# Inicialización de MySQL
-mysql = MySQL(app)
+def get_db_connection():
+    return mysql.connector.connect(**db_config)
 
 # Rutas de la aplicación
 @app.route('/')
 def index():
-    # Ejemplo de uso de la base de datos: obtener datos de la tabla 'roles'
-    cursor = mysql.connection.cursor()
+    connection = get_db_connection()
+    cursor = connection.cursor()
     cursor.execute("SELECT * FROM roles")
     roles = cursor.fetchall()
     cursor.close()
+    connection.close()
     return render_template('index.html', roles=roles)
 
 if __name__ == '__main__':
